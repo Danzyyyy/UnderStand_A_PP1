d24122a048bb18439a72ba890a017bccd1e19697
rayhannfd <raihanfh7@gmail.com>
2025-06-03 23:16:56 +0700

    feat: add IdGenerator utility for auto-generating ticket IDs

--- src/entity/Tiket.java
+++ src/entity/Tiket.java
@@ -1,13 +1,15 @@
 package entity;
 
+import utils.IdGenerator;
+
 public class Tiket {
     private String id;
     private String nama;
     private String rute;
     private String tanggal;
 
-    public Tiket(String id, String nama, String rute, String tanggal) {
-        this.id = id;
+    public Tiket(String nama, String rute, String tanggal) {
+        this.id = IdGenerator.generateId();
         this.nama = nama;
         this.rute = rute;
         this.tanggal = tanggal;
