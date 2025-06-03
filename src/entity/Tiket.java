e37b509917ec5807e8317fe69bd7ead901cf87cb
rayhannfd <raihanfh7@gmail.com>
2025-06-03 22:32:49 +0700

    feat: add Tiket class for linked list implementation

--- src/entity/Tiket.java
+++ src/entity/Tiket.java
@@ -1,5 +1,38 @@
-package Entity;
+package entity;
 
 public class Tiket {
+    private String id;
+    private String nama;
+    private String rute;
+    private String tanggal;
 
+    public Tiket(String id, String nama, String rute, String tanggal) {
+        this.id = id;
+        this.nama = nama;
+        this.rute = rute;
+        this.tanggal = tanggal;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public String getNama() {
+        return nama;
+    }
+    public String getRute() {
+        return rute;
+    }
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public void setRute(String rute) {
+        this.rute = rute;
+    }
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
 }
